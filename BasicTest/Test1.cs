c0482b75db4d73e8effa6e67d6f0a5f0fc7a921e
lawkk <lawkktd@gmail.com>
2024-03-22 18:17:08 +0530

    added tests

---
+++ BasicTest/Test1.cs
@@ -0,0 +1,46 @@
+﻿using BasicWeatherApi;
+using BasicWeatherApi.Controllers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTest
+{
+    public class Test1 : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public Test1(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_ReturnsWeatherForecast()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/WeatherForecast");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+
+            var forecasts = await response.Content.ReadFromJsonAsync<WeatherForecast[]>();
+            Assert.NotNull(forecasts);
+            Assert.NotEmpty(forecasts);
+
+            foreach (var forecast in forecasts)
+            {
+                Assert.InRange(forecast.TemperatureC, -20, 55);
+            }
+        }
+    }
+}
---
+++ BasicTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BasicTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
